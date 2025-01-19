dc319ebcd60816909f87aa1e73bb24b3a3dee8e9
Jagannath behera <jagannathbehera@oramahealth.in>
2025-01-19 22:32:37 +0530

    Modify artcle section

--- tailwind.config.js
+++ tailwind.config.js
@@ -99,10 +99,15 @@ export default {
             height: "0",
           },
         },
+        "color-blink": {
+          "0%, 100%": { color: "white" }, // Start and end color
+          "50%": { color: "yellow" }, // Middle color
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "color-blink": "color-blink 1.5s infinite",
       },
     },
   },
