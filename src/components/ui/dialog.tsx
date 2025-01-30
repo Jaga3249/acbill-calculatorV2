b1fc58d3515aca772d6e0dd7ed3a7be01942ef3e
Jagannath behera <jagannathbehera@oramahealth.in>
2025-01-30 08:01:47 +0530

    modify home page ui

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-// add
+// addgd
 import { cn } from "@/lib/utils";
 
 const Dialog = DialogPrimitive.Root;
